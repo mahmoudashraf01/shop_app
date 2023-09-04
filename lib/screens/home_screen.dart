60583578a4cbffc5d9eb69bbcc18404bd2580d6b
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2023-09-04 19:01:39 +0300

    edit categories structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,51 +23,74 @@ class HomeScreen extends StatelessWidget {
                 topRight: Radius.circular(35),
               ),
             ),
-            child: Column(children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 15),
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: white,
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 5),
-                      height: 50,
-                      width: 300,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Search ..',
-                          icon: Icon(
-                            Icons.search,
-                            color: darkBlue,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: white,
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Search ..',
+                            icon: Icon(
+                              Icons.search,
+                              color: darkBlue,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Spacer(),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 20,
-                ),
-                child: Text(
-                  'Categoties',
-                  style: h3Bold.merge(
-                    TextStyle(color: darkBlue),
+                      Spacer(),
+                    ],
                   ),
                 ),
-              ),
-              //adding categories.
-              CategotiesWidget(),
-            ]),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                  child: Text(
+                    'Categoties',
+                    style: h3Bold.merge(
+                      TextStyle(color: darkBlue),
+                    ),
+                  ),
+                ),
+                //adding categories.
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      CategotiesWidget(
+                        image: AssetImage('assets/1.jpg'), data: 'Backpack',
+                      ),
+                      CategotiesWidget(
+                        image: AssetImage('assets/2.jpg'), data: 'Bracelet',
+                      ),
+                      CategotiesWidget(
+                        image: AssetImage('assets/3.jpg'), data: 'Bracelet',
+                      ),
+                      CategotiesWidget(
+                        image: AssetImage('assets/4.jpg'), data: 'Cool Jacket',
+                      ),
+                      CategotiesWidget(
+                        image: AssetImage('assets/5.jpg'), data: 'Hooded Shirt',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
--- lib/shared/widgets/categories_widgets.dart
+++ lib/shared/widgets/categories_widgets.dart
@@ -4,41 +4,38 @@ import 'package:shoping_strore/shared/themes/text.dart';
 
 // ignore: must_be_immutable
 class CategotiesWidget extends StatelessWidget {
-  CategotiesWidget({super.key,});
+  CategotiesWidget({super.key,required this.image,required this.data});
+  ImageProvider<Object> image;
+  String data;
+
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(children: [
-        for(int i=0; i<5; i++)
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            decoration: BoxDecoration(
-              color: white,
-              borderRadius: BorderRadius.circular(20),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      decoration: BoxDecoration(
+        color: white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image(
+            image: image,
+            width: 40,
+            height: 40,
+          ),
+          Text(
+            data,
+            style: title1.merge(
+              TextStyle(
+                color: darkBlue,
+              ),
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image(
-                  image: AssetImage('assets/$i.jpg'),
-                  width: 40,
-                  height: 40,
-                ),
-                Text(
-                  'Backpack',
-                  style: title1.merge(
-                    TextStyle(
-                      color: darkBlue,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          )
-      ]),
+          ),
+        ],
+      ),
     );
   }
 }
