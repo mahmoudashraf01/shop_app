71a958190c742f90d6064a9d21698fab07ce335f
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2023-09-04 17:32:07 +0300

    adding badge

--- lib/shared/themes/colors.dart
+++ lib/shared/themes/colors.dart
@@ -4,4 +4,6 @@ var white = Colors.white;
 var black = Colors.black;
 var black2 = Colors.black12;
 var black3 = Colors.black26;
-var darkBlue = Color.fromRGBO(5, 11, 16, 1);
+var darkBlue = Color.fromRGBO(14, 31, 43, 1);
+var ligthRed = Color.fromARGB(255, 174, 7, 7);
+var darkRed = Color.fromARGB(255, 146, 0, 0);
--- lib/shared/widgets/home_app_bar.dart
+++ lib/shared/widgets/home_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shoping_strore/shared/themes/colors.dart';
 import 'package:shoping_strore/shared/themes/text.dart';
+import 'package:badges/badges.dart' as badges;
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
@@ -28,7 +29,23 @@ class HomeAppBar extends StatelessWidget {
               ),
             ),
           ),
-          
+          Spacer(),
+          badges.Badge(
+            badgeContent: Text(
+              '5',
+              style: TextStyle(
+                color: white,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: Icon(
+                Icons.shopping_bag_rounded,
+                color: darkBlue,
+                size: 30,
+              ),
+            ),
+          ),
         ],
       ),
     );
